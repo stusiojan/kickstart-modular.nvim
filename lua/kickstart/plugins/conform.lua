b0bdd3a8b5c14283563ed27ba807010246c7d5cf
Jan Stusio <jan.stusio@protonmail.com>
2026-06-16 08:56:33 +0200

    fix: turn off js files formatting

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -16,7 +16,7 @@ return {
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, javascript = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
